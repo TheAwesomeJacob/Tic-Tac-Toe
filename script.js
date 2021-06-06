4b0a742307f9e293670978a0d29b2a0a28e59dab
Jacob <66869348+TheAwesomeJacob@users.noreply.github.com>
2021-06-06 20:15:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
-const space = document.querySelectorAll("[class*=square]");
-console.log(space);
-let img = new Image(150, 150);
-img.src = 'blackX.png'
+const label = document.querySelectorAll("[class*='col']");
+const col1 = document.querySelectorAll("[class*='col1']");
+const col2 = document.querySelectorAll("[class*='col2']");
+const col3 = document.querySelectorAll("[class*='col3']");
+
+console.log(label);
+console.log(col1);
+console.log(col2);
+console.log(col3);
 
 function appendX(value){
     value.innerText = "X";
 }
-function appnedCircle(value){
+function appendCircle(value){
     value.innerText = "O";
 }
 
-function circleOrX(value){
-    switch(value.innerText){
-        case "X":
-            appnedCircle(value);
-            console.log('Im workingX');
-            break;
-        case "O":
-            appendX(value);
-            console.log('Im workingO');
-            break;
-        default:
-            appnedCircle(value);
-            console.log('Im workingD');
-            break;
+let player = true;
 
+function circleOrX(value){
+    if(player === true){
+        appendX(value);
+        player = false;
+    }else if(player === false){
+        appendCircle(value);
+        player = true;
     }
 }
+function winner(){
+    
+}
 
-space.forEach( event =>{
+label.forEach( event =>{
     event.addEventListener('click', p =>{
+        if(p.target.innerText.length === 0){
         circleOrX(p.target);
+        }
     })
 })
  
